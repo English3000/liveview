c07a2feb345457fa8f75c7ed4f828238a7698ca9
Alexander <english3000.org@gmail.com>
2020-12-10 15:48:33 -0600

    + search setup on- phx-submit

--- lib/live_view_studio_web/live/search_live.ex
+++ lib/live_view_studio_web/live/search_live.ex
@@ -4,7 +4,7 @@ defmodule LiveViewStudioWeb.SearchLive do
 
   @doc "sets initial state"
   def mount(_params, _session, socket) do
-    { :ok, assign(socket, [ zip: "", stores: Stores.list_stores() ]) }
+    { :ok, assign(socket, [ zip: "", results: [] ]) }
   end
 
   #mock data in stores.ex
@@ -14,9 +14,16 @@ defmodule LiveViewStudioWeb.SearchLive do
 
     <!-- nice pre included c s s -->
     <div id="search">
+      <form phx-submit="zip_search">
+        <input type="text" name="zip" value="<%= @zip %>" placeholder="zip code"
+                autofocus/>
+
+        <button type="submit"><img src="images/search.svg"/></button>
+      </form>
+
       <div class="stores">
         <ul>
-          <%= for store <- @stores do %><li>
+          <%= for store <- @results do %><li>
             <div class="first-line">
               <div class="name">
                 <%= store.name %>
@@ -47,5 +54,9 @@ defmodule LiveViewStudioWeb.SearchLive do
       </div>
     </div>
     """
+  end
+
+  def handle_event("zip_search", _, socket) do
+
   end
 end
